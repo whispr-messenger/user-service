1e593068acc72ed567af1ba375155a7b2c6e824e
Gabriel Lopez <gabriel.lopez@epitech.eu>
2026-01-15 19:46:29 +0100

    docker: fix dev container entrypoint

--- docker/dev/entrypoint.sh
+++ docker/dev/entrypoint.sh
@@ -2,13 +2,6 @@
 
 set -e
 
-# Install dependencies only when needed:
-# - if node_modules does not exist, or
-# - if package-lock.json is newer than node_modules (dependencies changed).
-if [ ! -d node_modules ]; then
-  npm install
-elif [ -f package-lock.json ] && [ package-lock.json -nt node_modules ]; then
-  npm install
-fi
+npm install
 
 npm run start:dev -- -b swc
