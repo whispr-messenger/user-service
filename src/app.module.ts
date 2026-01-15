a9001fb270a62b87df1a6a52a742cf72f75bb11d
Gabriel Lopez <gabriel.lopez@epitech.eu>
2026-01-15 19:54:15 +0100

    refactor: extract TypeORM config in a separate file

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,14 +4,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { DatabaseConfig } from './config/database.config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { PrivacyModule } from './privacy/privacy.module';
-import { ContactsModule } from './contacts/contacts.module';
-import { BlockedUsersModule } from './blocked-users/blocked-users.module';
-import { UserSearchModule } from './search/user-search.module';
-import { GroupsModule } from './groups/groups.module';
+// import { UsersModule } from './users/users.module';
+// import { PrivacyModule } from './privacy/privacy.module';
+// import { ContactsModule } from './contacts/contacts.module';
+// import { BlockedUsersModule } from './blocked-users/blocked-users.module';
+// import { UserSearchModule } from './search/user-search.module';
+// import { GroupsModule } from './groups/groups.module';
 import { CacheModule } from '@nestjs/cache-manager';
 import { cacheModuleAsyncOptions } from './cache.config';
+import { typeOrmModuleAsyncOptions } from './typeorm.config';
 
 @Module({
   imports: [
@@ -19,16 +20,14 @@ import { cacheModuleAsyncOptions } from './cache.config';
       isGlobal: true,
       envFilePath: ['.env.development', '.env.local', '.env'],
     }),
-    TypeOrmModule.forRootAsync({
-      useClass: DatabaseConfig,
-    }),
+    TypeOrmModule.forRootAsync(typeOrmModuleAsyncOptions),
     CacheModule.registerAsync(cacheModuleAsyncOptions),
-    UsersModule,
-    PrivacyModule,
-    ContactsModule,
-    BlockedUsersModule,
-    UserSearchModule,
-    GroupsModule,
+    // UsersModule,
+    // PrivacyModule,
+    // ContactsModule,
+    // BlockedUsersModule,
+    // UserSearchModule,
+    // GroupsModule,
   ],
   controllers: [AppController],
   providers: [AppService, DatabaseConfig],
