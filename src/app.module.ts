23277795c8869f5ffced48f0eeb0f23328a35dc7
Gabriel Lopez <gabriel.lopez@epitech.eu>
2026-01-15 20:03:26 +0100

    config: AppModule mount only stricly necessary modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { DatabaseConfig } from './config/database.config';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { CacheModule } from '@nestjs/cache-manager';
+import { cacheModuleAsyncOptions } from './cache.config';
+import { typeOrmModuleAsyncOptions } from './typeorm.config';
+import { HealthModule } from './health/health.module';
+
 // import { UsersModule } from './users/users.module';
 // import { PrivacyModule } from './privacy/privacy.module';
 // import { ContactsModule } from './contacts/contacts.module';
 // import { BlockedUsersModule } from './blocked-users/blocked-users.module';
 // import { UserSearchModule } from './search/user-search.module';
 // import { GroupsModule } from './groups/groups.module';
-import { CacheModule } from '@nestjs/cache-manager';
-import { cacheModuleAsyncOptions } from './cache.config';
-import { typeOrmModuleAsyncOptions } from './typeorm.config';
 
 @Module({
   imports: [
@@ -22,6 +21,7 @@ import { typeOrmModuleAsyncOptions } from './typeorm.config';
     }),
     TypeOrmModule.forRootAsync(typeOrmModuleAsyncOptions),
     CacheModule.registerAsync(cacheModuleAsyncOptions),
+    HealthModule,
     // UsersModule,
     // PrivacyModule,
     // ContactsModule,
@@ -29,7 +29,5 @@ import { typeOrmModuleAsyncOptions } from './typeorm.config';
     // UserSearchModule,
     // GroupsModule,
   ],
-  controllers: [AppController],
-  providers: [AppService, DatabaseConfig],
 })
-export class AppModule {}
+export class AppModule { }
