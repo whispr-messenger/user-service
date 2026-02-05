93f48931086601a1b1a24ac6c15fcaf5faa27f50
Gabriel Lopez <gabriel.lopez@epitech.eu>
2026-02-05 19:38:49 +0100

    fix(redis): add error listeners to prevent crashes

--- src/cache.config.ts
+++ src/cache.config.ts
@@ -1,4 +1,5 @@
 import { CacheModuleAsyncOptions, CacheOptions } from '@nestjs/cache-manager';
+import { Logger } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import KeyvRedis from '@keyv/redis';
 
@@ -7,8 +8,15 @@ function cacheModuleOptionsFactory(configService: ConfigService): CacheOptions {
 	const redis_port = configService.get('REDIS_PORT', 6379);
 	const redis_url = `redis://${redis_host}:${redis_port}`;
 
+
+
+	const keyvRedis = new KeyvRedis(redis_url);
+	keyvRedis.on('error', (err) => {
+		Logger.error('Redis connection error', err, 'CacheModule');
+	});
+
 	return {
-		stores: [new KeyvRedis(redis_url)],
+		stores: [keyvRedis],
 		ttl: 900,
 		max: 1000,
 	};
